6d7452c54987d8ff9fd5f892608541490fa70613
DaniVarious <thorsten_helis@gmx.de>
2024-05-16 10:14:26 +0200

    Add files via upload

---
+++ Pronatur Biomarkt GmbH/LoadingScreen.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Pronatur_Biomarkt_GmbH
+{
+    partial class LoadingScreen
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LoadingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblprogress = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadingBarTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadingProgressBar
+            // 
+            this.LoadingProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadingProgressBar.Location = new System.Drawing.Point(12, 190);
+            this.LoadingProgressBar.Name = "LoadingProgressBar";
+            this.LoadingProgressBar.Size = new System.Drawing.Size(694, 33);
+            this.LoadingProgressBar.TabIndex = 0;
+            this.LoadingProgressBar.Value = 50;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(303, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loading....";
+            // 
+            // lblprogress
+            // 
+            this.lblprogress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblprogress.AutoSize = true;
+            this.lblprogress.Location = new System.Drawing.Point(366, 169);
+            this.lblprogress.Name = "lblprogress";
+            this.lblprogress.Size = new System.Drawing.Size(21, 13);
+            this.lblprogress.TabIndex = 2;
+            this.lblprogress.Text = "0%";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Pronatur_Biomarkt_GmbH.Properties.Resources.GroceryImg;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(694, 149);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoadingBarTimer
+            // 
+            this.LoadingBarTimer.Tick += new System.EventHandler(this.LoadingBarTimer_Tick);
+            // 
+            // LoadingScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 235);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblprogress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoadingProgressBar);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProNatur Biomarkt GmbH";
+            this.Load += new System.EventHandler(this.LoadingScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar LoadingProgressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblprogress;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer LoadingBarTimer;
+    }
+}
+
---
+++ Pronatur Biomarkt GmbH/LoadingScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pronatur_Biomarkt_GmbH
+{
+    public partial class LoadingScreen : Form
+    {
+        private int loadingBarValue;
+        
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            LoadingBarTimer.Start();
+
+        }
+
+        private void LoadingBarTimer_Tick(object sender, EventArgs e)
+        {
+            loadingBarValue++;
+            
+            LoadingProgressBar.Value = loadingBarValue;
+            lblprogress.Text = loadingBarValue.ToString();
+
+            if (loadingBarValue >= 100)
+            {
+                LoadingBarTimer.Stop();
+            }
+        }
+
+
+    }
+}
